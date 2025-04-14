4294e4a73b91e605899cc9f9a6418d98df9e4742
Francisco9djul <Fran9dj@outlook.com>
2025-04-14 15:26:30 -0300

    [Parte 3.8] Espacios de nombres en los nombres de URL

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -4,22 +4,25 @@ from django.http import HttpResponse
 
 from .models import Question
 
-from django.http import Http404
-# Create your views here.
 
+# Create your views here.
+#Vista principal
 def index(request):
 	latest_question_list = Question.objects.order_by("-pub_date")[:5]
 	context = {"latest_question_list": latest_question_list}
 	return render(request, "polls/index.html", context)
 
+#vista muestra el detalle de una pregunta
 def detail(request, question_id):
 	
 		question = get_object_or_404(Question, pk=question_id) #me devuelve el objeto o un error 404 si no existe
 		return render(request, "polls/detail.html", {"question": question})
 
+#vista de resultados
 def results(request, question_id):
 	response = "You're looking at the results of question %s."
 	return HttpResponse(response % question_id)
 
+#vista para votar
 def vote(request, question_id):
 	return HttpResponse("You're voting on questions %s." % question_id)
