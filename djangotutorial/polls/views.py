6c5928a04bebfde5e8fc30eed06027877dea60f1
Francisco9djul <Fran9dj@outlook.com>
2025-04-14 09:45:08 -0300

    [Parte 3.5] Un atajo: get_object_or_404()

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from django.http import HttpResponse
 
@@ -13,11 +13,9 @@ def index(request):
 	return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-	try:
-		question = Question.objects.get(pk=question_id)
-	except Question.DoesNotExist:
-		raise Http404("Question does not exist")
-	return render(request, "polls/detail.html", {"question": question})
+	
+		question = get_object_or_404(Question, pk=question_id) #me devuelve el objeto o un error 404 si no existe
+		return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
 	response = "You're looking at the results of question %s."
