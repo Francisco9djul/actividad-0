94ed9b534d73ed76d6d2b13b911d729cb577b17e
Francisco9djul <Fran9dj@outlook.com>
2025-04-16 20:12:23 -0300

    [Parte 5.4] Mejorando nuestra view

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import Choice, Question
 from django.views import generic
+from django.utils import timezone
 
 
 # Create your views here.
@@ -18,7 +19,11 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
     
 #vista muestra el detalle de una pregunta
 class DetailView(generic.DetailView):
